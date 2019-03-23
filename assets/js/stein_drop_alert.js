fda3298dc9c3e7c67bf51fa83fff4df0fce15c88
steinpyre <lufufoboro@spindl-e.com>
2019-03-23 08:13:15 -0400

    Added alert sound

--- assets/js/stein_drop_alert.js
+++ assets/js/stein_drop_alert.js
@@ -1,3 +1,6 @@
+var alert = new Audio("https://steinpyre.github.io/Stein-Plugin/assets/mp3/alert_ping.mp3");
+alert.volume = 0.3;
+//alert.play();
 (function($) {
 	jQuery.noConflict();
 	console.log("stein_dropalert.js: Script loaded");
@@ -5,15 +8,26 @@
 	var observer = new MutationObserver(function( mutations ) {
 		mutations.forEach(function( mutation ) {
 			var newNodes = mutation.addedNodes;
+			var _playAlert = false;
 			if( newNodes !== null ) {
 				var nodes = $(newNodes);
 				nodes.each(function() {
 					var node = $(this);
 					if(node.hasClass("stein-dropped-item-container")){
-						console.log("stein_dropalert.js: Item dropped");
+						_playAlert = true;
+						/*
+						var childNode = node.find(".stein-dropped-item");
+						if(childNode.hasClass("stein-dropped-item-common"));
+						if(childNode.hasClass("stein-dropped-item-uncommon"));
+						if(childNode.hasClass("stein-dropped-item-rare"));
+						if(childNode.hasClass("stein-dropped-item-epic"));
+						if(childNode.hasClass("stein-dropped-item-legendary"));
+						*/
+						//console.log("stein_dropalert.js: Item dropped");
 					}
 				});
 			}
+			if(playAlert) alert.play();
 		});
 	});
 	// Configuration of the observer:
