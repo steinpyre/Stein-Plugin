c1bb0239778c0597076b9576b1e842d0fab00a5b
steinpyre <lufufoboro@spindl-e.com>
2019-03-27 00:57:28 -0400

    New js loader

--- stein_plugin.js
+++ stein_plugin.js
@@ -1,12 +1,11 @@
+/*
 function loadScript(url, callback){
     // Adding the script tag to the head as suggested before
     var head = document.getElementsByTagName('head')[0];
     var script = document.createElement('script');
     script.type = 'text/javascript';
     script.src = url;
-
-    // Then bind the event to the callback function.
-    // There are several events for cross browser compatibility.
+	
     script.onreadystatechange = callback;
     script.onload = callback;
 
@@ -17,7 +16,6 @@ var cssPath = "https://steinpyre.github.io/Stein-Plugin/assets/css/stein_plugin.
 loadScript("//code.jquery.com/jquery-latest.min.js", function(){
 	//alert("jquery loaded");
 	(function($) {
-		loadScript("https://steinpyre.github.io/Stein-Plugin/vendors/cookie_js/js.cookie.min.js", function(){});
 		loadScript("https://steinpyre.github.io/Stein-Plugin/assets/js/stein_chat_tabs.js", function(){});
 		loadScript("https://steinpyre.github.io/Stein-Plugin/assets/js/stein_ignore.js", function(){});
 		loadScript("https://steinpyre.github.io/Stein-Plugin/assets/js/stein_timestamp.js", function(){});
@@ -26,3 +24,46 @@ loadScript("//code.jquery.com/jquery-latest.min.js", function(){
 		if (!$("link[href='"+cssPath+"']").length) $('<link href="'+cssPath+'" rel="stylesheet">').appendTo("head");
 	})(jQuery);
 });
+*/
+var cssPath = "https://steinpyre.github.io/Stein-Plugin/assets/css/stein_plugin.css";
+var Loader = function () { }
+Loader.prototype = {
+    require: function (scripts, callback) {
+        this.loadCount      = 0;
+        this.totalRequired  = scripts.length;
+        this.callback       = callback;
+
+        for (var i = 0; i < scripts.length; i++) {
+            this.writeScript(scripts[i]);
+        }
+    },
+    loaded: function (evt) {
+        this.loadCount++;
+
+        if (this.loadCount == this.totalRequired && typeof this.callback == 'function') this.callback.call();
+    },
+    writeScript: function (src) {
+        var self = this;
+        var s = document.createElement('script');
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = src;
+        s.addEventListener('load', function (e) { self.loaded(e); }, false);
+        var head = document.getElementsByTagName('head')[0];
+        head.appendChild(s);
+    }
+}
+var mLoader = new Loader();
+mLoader.require([
+	"//code.jquery.com/jquery-latest.min.js",
+	"https://steinpyre.github.io/Stein-Plugin/vendors/cookie_js/js.cookie.min.js",
+	], 
+	function() {
+		mLoader.require(["https://steinpyre.github.io/Stein-Plugin/assets/js/stein_chat_tabs.js"], function(){})
+		mLoader.require(["https://steinpyre.github.io/Stein-Plugin/assets/js/stein_ignore.js"], function(){})
+		mLoader.require(["https://steinpyre.github.io/Stein-Plugin/assets/js/stein_timestamp.js"], function(){})
+		mLoader.require(["https://steinpyre.github.io/Stein-Plugin/assets/js/stein_drop_alert.js"], function(){})
+		//console.log('All Scripts Loaded');
+		if (!$("link[href='"+cssPath+"']").length) $('<link href="'+cssPath+'" rel="stylesheet">').appendTo("head");
+	}
+);
